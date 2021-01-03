9558a6aab870ff94644e00c93ed46511e59fe09f
TeslaHua <wangzhonghua_cnu@163.com>
2021-01-03 21:50:24 +0800

    add node-key for el-tree element

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,13 +1,30 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node = 'false' show-checkbox="true">
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+    :expand-on-click-node="false"
+    show-checkbox="true"
+    node-key="catId"
+  >
     <!-- render-content 增删分类数据 -->
     <span class="custom-tree-node" slot-scope="{ node, data }">
       <span>{{ node.label }}</span>
       <span>
-        <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(data)"
+        >
           Append
         </el-button>
-        <el-button v-if="node.childNodes.length==0" type="text" size="mini" @click="() => remove(node, data)">
+        <el-button
+          v-if="node.childNodes.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >
           Delete
         </el-button>
       </span>
@@ -43,10 +60,10 @@ export default {
     },
     // 分类数据的增删方法
     append(data) {
-        console.log(data)
+      console.log(data);
     },
     remove(node, data) {
-        console.log(node,data)
+      console.log(node, data);
     },
   },
   //生命周期 - 创建完成 (当作访问当前 this 实例)
